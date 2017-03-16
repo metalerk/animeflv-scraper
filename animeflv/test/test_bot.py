dd018092c2a95d9b55cc1585bc1b06047740b6a7
metalerk <rodriguezjluis0@gmail.com>
2017-03-16 10:50:37 -0600

    Get last episodes

--- animeflv/test/test_bot.py
+++ animeflv/test/test_bot.py
@@ -7,8 +7,10 @@ class TestBot(unittest.TestCase):
     def test_get_last_episodes(self):
         bot = BotAnimeFLV()
         last_episodes = bot.get_last_episodes()
-        response = json.dumps(last_episodes)
-        pprint(last_episodes)
+        self.assertEqual(type(last_episodes), list)
+        self.assertGreater(last_episodes.__len__(), 0)
+        #response = json.dumps(last_episodes)
+        #pprint(last_episodes)
 
 if __name__ == '__main__':
     unittest.main()
